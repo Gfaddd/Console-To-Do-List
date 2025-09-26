70e90368c2d1e39c7ea399556b4464d7c347d84c
IvanSakuta <vanasakuta@gmail.com>
2025-09-26 10:50:22 +0300

    Develop Task

---
+++ Task.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_To_Do_List
+{
+    internal class Task
+    {
+        private string name;
+        private bool isCompleted;
+        private DateTime completingTime;
+        public void compleatTask()
+        {
+            isCompleted = true;
+        }
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("value");
+                }
+            }
+        }
+        public DateTime CompletingTime { get { return completingTime; } }
+    }
+}
